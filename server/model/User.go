f90b128b9f09c022363b8510e5ef6fd5c2889462
pasly0920 <inwoo920621@gmail.com>
2023-03-08 23:31:52 +0900

    feat: user domain model 추가

---
+++ server/model/User.go
@@ -0,0 +1,19 @@
+package model
+
+type User struct {
+	ID           uint64 `gorm:"primaryKey"`
+	Username     string `gorm:"not null"`
+	Name         string `gorm:"not null"`
+	Nickname     string `gorm:"not null"`
+	AvatarID     uint64
+	PhoneNumber  string
+	StudentYear  int
+	StudentGroup string
+	MajorCode    int
+}
+
+// user table name user to users
+
+func (u *User) TableName() string {
+	return "users"
+}
